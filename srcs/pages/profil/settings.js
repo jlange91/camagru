f5741cd2546f1b8c3113029752a180449d14f286
Julien LANGE <jlange@e1r9p19.42.fr>
2019-05-20 18:38:04 +0200

    modif mysql container

--- srcs/pages/profil/settings.js
+++ srcs/pages/profil/settings.js
@@ -4,9 +4,18 @@ settings.setAttribute("id", "profil-settings");
 
 function loadSettingsProfil() {
   settings.innerHTML = '\
-  <div id="profil-settings-tittle-username">Change username.</div>\
-  <div id="profil-settings-tittle-cp">Change password.</div>\
-  <div id="profil-settings-tittle-mail">Change mail.</div>\
+  <ul class="menu-list">\
+    <li><a>Team Settings</a></li>\
+    <li>\
+      <a class="is-active">Manage Your Team</a>\
+      <div>\
+      rien a foutre\
+      </div>\
+    </li>\
+    <li><a>Invitations</a></li>\
+    <li><a>Cloud Storage Environment Settings</a></li>\
+    <li><a>Authentication</a></li>\
+  </ul>\
   ';
   profil.appendChild(settings);
 }
