c7fcc0510329ba71ae09e6eafdc6a6516ecc9b90
julien lange <julienlange@MacBook-Pro-de-julien.local>
2019-06-03 01:29:59 +0200

    change launch script

--- launch.sh
+++ launch.sh
@@ -2,9 +2,12 @@
 mkdir srcs/assets/publication
 chmod 777 srcs/assets/publication
 docker-machine create --driver virtualbox Camagru
+# Need DOCKER_IP, DB_USER, DB_PASSWORD, MAIL_USER and MAIL_PASSWORD to work
 export DOCKER_IP=$(docker-machine ip Camagru)
 export DB_USER=root
 export DB_PASSWORD=root
+export MAIL_USER=jlangecamagru@gmail.com
+export MAIL_PASSWORD=camagru42
 eval $(docker-machine env Camagru)
 docker-compose build
 docker-compose up -d
