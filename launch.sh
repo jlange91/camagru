5996ffc68cd8b4ddf7e5b71dc56cd82d158dc13e
Julien LANGE <jlange@e3r4p9.42.fr>
2019-06-03 15:21:19 +0200

    secure autodeploy

--- launch.sh
+++ launch.sh
@@ -1,16 +1,26 @@
 # Need docker-machine, docker-compose, virtualbox
+# Need DOCKER_IP, DB_USER, DB_PASSWORD, MAIL_USER and MAIL_PASSWORD to work
+printf "DB_USER="
+read DB_USER
+printf "DB_PASSWORD="
+read -s DB_PASSWORD
+echo ""
+printf "MAIL_USER="
+read MAIL_USER
+printf "MAIL_PASSWORD="
+read -s MAIL_PASSWORD
+echo ""
+export DB_USER
+export DB_PASSWORD
+export MAIL_USER
+export MAIL_PASSWORD
 mkdir srcs/assets/publication
 chmod 777 srcs/assets/publication
 docker-machine create --driver virtualbox Camagru
-# Need DOCKER_IP, DB_USER, DB_PASSWORD, MAIL_USER and MAIL_PASSWORD to work
 export DOCKER_IP=$(docker-machine ip Camagru)
-export DB_USER=root
-export DB_PASSWORD=root
-export MAIL_USER=jlangecamagru@gmail.com
-export MAIL_PASSWORD=camagru42
 eval $(docker-machine env Camagru)
 docker-compose build
 docker-compose up -d
-sleep 10
-test=`docker ps | grep camagru_php | awk '{print $1}'`
-docker exec $test php /var/www/config/setup.php
+sleep 30
+phpContainer=`docker ps | grep camagru_php | awk '{print $1}'`
+docker exec $phpContainer php /var/www/config/setup.php
--- srcs/config/setup.php
+++ srcs/config/setup.php
@@ -2,53 +2,56 @@
 
 $path = '/var/www';
 include("{$path}/config/database.php");
+$setupFile = "{$path}/config/setup.sql";
 
-$db->exec('DROP TABLE IF EXISTS camagru.Users');
+if (file_exists($setupFile)) {
+  $setup = file_get_contents($setupFile);
+  $db->exec($setup);
+}
+else {
+  $db->exec('DROP TABLE IF EXISTS camagru.Users');
 
-$db->exec('CREATE TABLE IF NOT EXISTS Users (
-    email             VARCHAR(255) NOT NULL,
-    username          VARCHAR(40) NOT NULL,
-    password          VARCHAR(512) NOT NULL,
-    mailHash          VARCHAR(256) NOT NULL,
-    resetPasswordHash VARCHAR(256) NOT NULL,
-    sendMailDate      DATETIME NOT NULL,
-    sendMailComment   BOOLEAN NOT NULL DEFAULT 1,
-    completed         BOOLEAN NOT NULL DEFAULT 0,
-    PRIMARY KEY(username)
-)');
+  $db->exec('CREATE TABLE IF NOT EXISTS Users (
+      email             VARCHAR(255) NOT NULL,
+      username          VARCHAR(40) NOT NULL,
+      password          VARCHAR(512) NOT NULL,
+      mailHash          VARCHAR(256) NOT NULL,
+      resetPasswordHash VARCHAR(256) NOT NULL,
+      sendMailDate      DATETIME NOT NULL,
+      sendMailComment   BOOLEAN NOT NULL DEFAULT 1,
+      completed         BOOLEAN NOT NULL DEFAULT 0,
+      PRIMARY KEY(username)
+  )');
 
-$db->exec('DROP TABLE IF EXISTS camagru.Publications');
+  $db->exec('DROP TABLE IF EXISTS camagru.Publications');
 
-$db->exec('CREATE TABLE IF NOT EXISTS Publications (
-    date      DATETIME NOT NULL,
-    path      VARCHAR(255) NOT NULL,
-    username  VARCHAR(40) NOT NULL,
-    comment   VARCHAR(255) NOT NULL,
-    uniqid    VARCHAR(255) NOT NULL,
-    PRIMARY KEY(uniqid)
-)');
+  $db->exec('CREATE TABLE IF NOT EXISTS Publications (
+      date      DATETIME NOT NULL,
+      path      VARCHAR(255) NOT NULL,
+      username  VARCHAR(40) NOT NULL,
+      comment   VARCHAR(255) NOT NULL,
+      uniqid    VARCHAR(255) NOT NULL,
+      PRIMARY KEY(uniqid)
+  )');
 
-$db->exec('DROP TABLE IF EXISTS camagru.Comments');
+  $db->exec('DROP TABLE IF EXISTS camagru.Comments');
 
-$db->exec('CREATE TABLE IF NOT EXISTS Comments (
-    date            DATETIME NOT NULL,
-    comment         VARCHAR(255) NOT NULL,
-    username        VARCHAR(40) NOT NULL,
-    publicationId   VARCHAR(255) NOT NULL,
-    uniqid          VARCHAR(255) NOT NULL,
-    PRIMARY KEY(uniqid)
-)');
+  $db->exec('CREATE TABLE IF NOT EXISTS Comments (
+      date            DATETIME NOT NULL,
+      comment         VARCHAR(255) NOT NULL,
+      username        VARCHAR(40) NOT NULL,
+      publicationId   VARCHAR(255) NOT NULL,
+      uniqid          VARCHAR(255) NOT NULL,
+      PRIMARY KEY(uniqid)
+  )');
 
-$db->exec('DROP TABLE IF EXISTS camagru.Likes');
+  $db->exec('DROP TABLE IF EXISTS camagru.Likes');
 
-$db->exec('CREATE TABLE IF NOT EXISTS Likes (
-    publicationId   VARCHAR(255) NOT NULL,
-    username        VARCHAR(40) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (publicationId,username)
-)');
-
-$setup = file_get_contents('setup.sql');
-
-$db->exec($setup);
+  $db->exec('CREATE TABLE IF NOT EXISTS Likes (
+      publicationId   VARCHAR(255) NOT NULL,
+      username        VARCHAR(40) NOT NULL,
+      CONSTRAINT id PRIMARY KEY (publicationId,username)
+  )');
+}
 
 ?>
